e7dbef6f825ecebdedae3df15ef93dd219abf894
Sounak Guin <sounak.guin@gmail.com>
2024-08-02 14:48:36 +0530

    final

--- src/Components/Movie/SingleMovieDetails.jsx
+++ src/Components/Movie/SingleMovieDetails.jsx
@@ -114,7 +114,8 @@ const SingleMovieDetails = () => {
 
   const responsive = {
     superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 6 },
-    desktop: { breakpoint: { max: 3000, min: 1024 }, items: 5 },
+    desktop: { breakpoint: { max: 3000, min: 2000 }, items: 5 },
+    laptop: { breakpoint: { max: 2000, min: 1024 }, items: 6 },
     tablet: { breakpoint: { max: 1024, min: 464 }, items: 2 },
     mobile: { breakpoint: { max: 464, min: 0 }, items: 1 },
   };
