9e90b98edf9e0026fe705cf2bf19cfdfebc7bcf3
Sounak Guin <sounak.guin@gmail.com>
2024-08-02 14:40:36 +0530

    final

--- src/Components/Movie/SingleMovieDetails.jsx
+++ src/Components/Movie/SingleMovieDetails.jsx
@@ -113,7 +113,7 @@ const SingleMovieDetails = () => {
   const isDesktopOrLarger = useMediaQuery({ minWidth: 1024 });
 
   const responsive = {
-    superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 7 },
+    superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 6 },
     desktop: { breakpoint: { max: 3000, min: 1024 }, items: 6 },
     tablet: { breakpoint: { max: 1024, min: 464 }, items: 2 },
     mobile: { breakpoint: { max: 464, min: 0 }, items: 1 },
--- src/Components/TV/SingleTVpage.jsx
+++ src/Components/TV/SingleTVpage.jsx
@@ -6,7 +6,7 @@ import "react-multi-carousel/lib/styles.css";
 const Carousel = lazy(() => import("react-multi-carousel"));
 
 const responsive = {
-  superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 7 },
+  superLargeDesktop: { breakpoint: { max: 4000, min: 3000 }, items: 6 },
   desktop: { breakpoint: { max: 3000, min: 1024 }, items: 6 },
   tablet: { breakpoint: { max: 1024, min: 464 }, items: 2 },
   mobile: { breakpoint: { max: 464, min: 0 }, items: 1 },
